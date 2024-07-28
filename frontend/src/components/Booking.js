03cd34b82900f24d7bd27275630ebe0ac4aa95c2
Ron Chim <ronrchim@gmail.com>
2024-07-28 15:09:07 -0400

    Fix button to navigate user to next page

--- frontend/src/components/Booking.js
+++ frontend/src/components/Booking.js
@@ -1,15 +1,24 @@
 import React, { useState, useEffect } from 'react';
 import './Booking.css';
 import MyCalendar from './MyCalendar.js'
+import { useNavigate } from 'react-router-dom';
 
-function Booking() {
+function Booking({ selectedDate, setSelectedDate }) {
   // Interact with backend
   const apiUrl = process.env.REACT_APP_PUBLIC_API_URL;
   
   // Change selected date on user click
   const [date, setDate] = useState(new Date());
   function onClickDay(date) {
+    setSelectedDate(date);    // selected date will be passed to another component
     setDate(date);
+  }
+
+  const navigate = useNavigate();
+  function onClickNext() {
+    if (selectedDate !== null) {
+      navigate("/next");
+    }
   }
   
   return (
@@ -20,7 +29,7 @@ function Booking() {
           <span>Next</span>
         </button>
       </form> */}
-      <button type="submit" className="next">
+      <button type="submit" className="next" onClick={onClickNext}>
           <span>Next</span>
       </button>
     </div>
